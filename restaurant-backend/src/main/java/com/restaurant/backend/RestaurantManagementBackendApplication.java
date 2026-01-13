f5c675f43090e2d4d196ab304aa12a4c5288f6ea
Vincenzo-Rojas <vincenzo.rojas.carrera@gmail.com>
2026-01-13 19:42:13 +0100

    Origin spring

---
+++ restaurant-backend/src/main/java/com/restaurant/backend/RestaurantManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagementBackendApplication.class, args);
+	}
+
+}
